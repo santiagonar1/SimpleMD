4e9a67edbf91995f483d9a766794838c33675df9
Santiago Narvaez <santiagonar1@gmail.com>
2022-02-11 17:25:03 +0100

    Add method to update the forces of all particles
    This is still an initial and naive implementation. The algorithm
    is serial, and there are some points where clone is used on the
    meantime.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 type Real = f64;
 
+#[derive(Clone)]
 struct Particle {
     m: Real,
     pos: [Real; 3],
@@ -66,6 +67,17 @@ impl Simulation {
             for d in 0..particle.v.len() {
                 particle.v[d] += a * (particle.f[d] + particle.f_old[d]);
             }
+        }
+    }
+
+    fn update_f(&mut self) {
+        let l = self.particles.len();
+        for i in 0..l {
+            let mut particle = self.particles[i].clone();
+            for j in (i + 1)..l {
+                include_forces(&mut particle, &mut self.particles[j]);
+            }
+            self.particles[i] = particle;
         }
     }
 }
@@ -137,5 +149,47 @@ mod tests {
             expected_new_v[d] = v[d] + a * (f[d] + f[d]);
         }
         assert_eq!(simulation.particles[0].v, expected_new_v);
+    }
+
+    #[test]
+    fn update_f() {
+        let pos1 = [1.0, 2.0, 3.0];
+        let v1 = [1.0, 2.0, 3.0];
+        let f1 = [1.0, 2.0, 3.0];
+        let m1 = 6.0;
+        let p1 = Particle::new(m1, pos1, v1, f1);
+
+        let pos2 = [1.0, 0.0, 5.0];
+        let v2 = [3.0, 2.0, 2.0];
+        let f2 = [4.0, 2.0, 3.0];
+        let m2 = 6.0;
+        let p2 = Particle::new(m2, pos2, v2, f2);
+
+        let delta_t = 1.0;
+        let mut simulation = Simulation {
+            particles: vec![p1, p2],
+            delta_t,
+        };
+
+        simulation.update_f();
+
+        let p1 = Particle::new(m1, pos1, v1, f1);
+        let p2 = Particle::new(m2, pos2, v2, f2);
+        let mut expected_new_f_p1 = [0.0, 0.0, 0.0];
+        let mut expected_new_f_p2 = [0.0, 0.0, 0.0];
+        let r: Real = p1
+            .pos
+            .iter()
+            .zip(p2.pos.iter())
+            .map(|(x1, x2)| (x2 - x1).powi(2))
+            .sum();
+        let f = (p1.m * p2.m) / (r.sqrt() * r);
+        for d in 0..expected_new_f_p1.len() {
+            expected_new_f_p1[d] = p1.f[d] + f * (p2.pos[d] - p1.pos[d]);
+            expected_new_f_p2[d] = p2.f[d] - f * (p2.pos[d] - p1.pos[d]);
+        }
+
+        assert_eq!(simulation.particles[0].f, expected_new_f_p1);
+        assert_eq!(simulation.particles[1].f, expected_new_f_p2);
     }
 }
