094a5373bafc12b68fc18d53badd189a14706461
Santiago Narvaez <santiagonar1@gmail.com>
2022-02-20 10:47:05 +0100

    Particle: Remove unnecessary range loop
    We can get both the index and the value with the enumerate method.
    That way it is clear that we need to traverse the values of
    the force and substract them from p2

--- src/particle.rs
+++ src/particle.rs
@@ -40,11 +40,10 @@ impl Particle {
 
 pub fn include_forces(p1: &mut Particle, p2: &mut Particle) {
     let f_p2_into_p1 = p1.add_f_from(p2);
-    for d in 0..p1.pos.len() {
-        p2.f[d] -= f_p2_into_p1[d];
+    for (d, force) in f_p2_into_p1.iter().enumerate() {
+        p2.f[d] -= force;
     }
 }
-
 
 #[cfg(test)]
 mod tests {
